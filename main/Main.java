90b064dd5f83cd321eeca3ea285b5d73fd397f36
Olavo Neves <olavo9neves@gmail.com>
2025-04-04 12:08:32 -0300

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -5,7 +5,6 @@ import br.com.compras.model.Compra;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
